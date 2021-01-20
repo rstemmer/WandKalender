d6cfbf2333b62121e79e87e9eb545e93b299788f
Ralf Stemmer <ralf.stemmer@gmx.net>
2021-01-20 15:27:02 +0100

    Configuration added

---
+++ WandKalender/config.js
@@ -0,0 +1,8 @@
+
+if(typeof window.WandKalender === "undefined")
+    window.WandKalender = new Object();
+
+window.WandKalender.serverurl = ""; // URL to the CalDAV server
+window.WandKalender.username  = ""; // User name to log into the CalDAV server
+window.WandKalender.password  = ""; // Password for authentication at the CalDAV server
+
