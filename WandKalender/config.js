8cc05907f39da01d6da9d98d88d67c9363e21dd6
Ralf Stemmer <ralf.stemmer@gmx.net>
2021-01-21 13:07:23 +0100

    The login information are now stored in the localStorage

--- WandKalender/config.js
+++ WandKalender/config.js
@@ -2,7 +2,3 @@
 if(typeof window.WandKalender === "undefined")
     window.WandKalender = new Object();
 
-window.WandKalender.serverurl = ""; // URL to the CalDAV server
-window.WandKalender.username  = ""; // User name to log into the CalDAV server
-window.WandKalender.password  = ""; // Password for authentication at the CalDAV server
-
