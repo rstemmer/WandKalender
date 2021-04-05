82b40f4990d917fb245e47d68ec352193c97ffa7
Ralf Stemmer <ralf.stemmer@gmx.net>
2021-04-05 10:39:05 +0200

    Handle byday rules

--- WandKalender/js/Event.js
+++ WandKalender/js/Event.js
@@ -83,42 +83,118 @@ class Event extends iCalParser
         // Create further entries if the event repeats
         if(eventinfos.hasOwnProperty("repeats"))
         {
-            // FREQ=DAILY;COUNT=5;INTERVAL=2
-            let rules     = eventinfos["repeats"];
-            let count     = parseInt(rules["COUNT"]);
+            let date  = begin;
+            let rules = eventinfos["repeats"];
+
             let interval;
-            if(rules["INTERVAL"] != undefined)
+            if("INTERVAL" in rules)
                 interval = parseInt(rules["INTERVAL"]);
             else
                 interval = 1;
             let frequency = rules["FREQ"];
-            let date      = begin;
 
-            for(let i=1; i<count; i++) // Start with 1, because 0 has already been processed
+            // Repeat N times
+            if("COUNT" in rules)
             {
-                switch(frequency)
-                {
-                    case "DAILY":   date.setDate(date.getDate() + interval  ); break;
-                    case "WEEKLY":  date.setDate(date.getDate() + interval*7); break;
-                    case "MONTHLY": date.setMonth(date.getMonth() + interval); break;
-                }
-
-                if(allday)
-                    entry = new CalendarEntry(name, new Date(date));
-                else
-                {
-                    end.setFullYear(date.getFullYear());
-                    end.setMonth(date.getMonth());
-                    end.setDate(date.getDate());
-                    entry = new CalendarEntry(name, new Date(date), new Date(end));
-                }
-
-                this.entries.push(entry);
+                let count = parseInt(rules["COUNT"]);
+                this.RepeatNTimes(name, frequency, interval, begin, end, allday, count);
+            }
+            // Repeat infinite times
+            else if("BYDAY" in rules)
+            {
+                let byday    = rules["BYDAY"];
+                let bysetpos = parseInt(rules["BYSETPOS"]);
+                this.RepeatByDay(name, frequency, interval, begin, end, allday, byday, bysetpos);
             }
         }
 
-        //window.console && console.log(entries);
         return this.entries;
+    }
+
+
+
+    RepeatNTimes(name, frequency, interval, begin, end, allday, count)
+    {
+        //window.console?.log(`REPEAD ${name} N times`);
+        let date = begin;
+        for(let i=1; i<count; i++) // Start with 1, because 0 has already been processed
+        {
+            switch(frequency)
+            {
+                case "DAILY":   date.setDate(date.getDate() + interval  ); break;
+                case "WEEKLY":  date.setDate(date.getDate() + interval*7); break;
+                case "MONTHLY": date.setMonth(date.getMonth() + interval); break;
+            }
+
+            this.AddEntry(name, allday, date, end);
+        }
+        return;
+    }
+
+
+
+    RepeatByDay(name, frequency, interval, begin, end, allday, byday, bysetpos)
+    {
+        // window.console?.log(`REPEAD ${name} by Day: f=${frequency}, i=${interval}, byday=${byday}, bysetpos=${bysetpos}`);
+        // Repeat until end of month
+        let now        = new Date();
+        let endofmonth = new Date();
+        endofmonth.setMonth(now.getMonth() + 1);
+
+        let offset;
+        if(typeof(bysetpos) === "number" && isNaN(bysetpos) == false)
+            offset = (bysetpos-1) * 7; // bysetpos defines the week of the month
+        else
+            offset = 0;
+
+        let daylut = {"SO":0, "MO":1, "TU":2, "WE":3, "TH":4, "FR":5, "SA":6};
+        let date   = begin;
+        while(date <= endofmonth)
+        {
+            if(frequency === "MONTHLY")
+            {
+                date.setMonth(date.getMonth() + interval);
+                date.setDate(1); // start at the begin of the month, then apply ByDay rule
+
+                // Apply  ByDay rule
+                let dayofweek = date.getDay();
+                let daytoset  = daylut[byday];
+                let distance  = daytoset - dayofweek;
+                if(distance < 0) distance += 7;
+                date.setDate(date.getDate() + distance + offset);
+            }
+            else if(frequency === "WEEKLY")
+            {
+                date.setDate(date.getDate() + interval*7);
+            }
+            else
+            {
+                window.console?.error(`a frequency of ${frequency} is not supported`);
+            }
+
+
+            this.AddEntry(name, allday, date, end);
+        }
+        return;
+    }
+
+
+
+    AddEntry(name, allday, date, end)
+    {
+        let entry;
+        if(allday)
+            entry = new CalendarEntry(name, new Date(date));
+        else
+        {
+            end.setFullYear(date.getFullYear());
+            end.setMonth(date.getMonth());
+            end.setDate(date.getDate());
+            entry = new CalendarEntry(name, new Date(date), new Date(end));
+        }
+
+        this.entries.push(entry);
+        return;
     }
 }
 
